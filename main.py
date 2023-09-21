b88e2ffc88635242704bc6e08dfe3885a0c25fc8
JE1532 <116882785+JE1532@users.noreply.github.com>
2023-09-21 18:03:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import numpy
 
-def forward_euler_integration(y_0, vec_deriv, delta):
+def forward_euler_integration(y_0, vec_deriv, delta, conv=None):
     x_arr = [0]
     y_arr = [y_0]
-    get_second_deriv = lambda y_vals: numpy.array([y[2] for y in y_vals])
-    while len(y_arr) < 100 or numpy.std(get_second_deriv(y_arr[-100:])) >= 0.01 * numpy.avg(get_second_deriv(y_arr[-100:][2])):
+    conv = len(y_0) - 1 if not conv else conv
+    get_conv_derivative = lambda y_vals: numpy.array([y[conv] for y in y_vals])
+    while len(y_arr) < 100 or numpy.std(get_conv_derivative(y_arr[-100:])) >= 0.01 * numpy.avg(get_conv_derivative(y_arr[-100:])):
         x_arr.append(x_arr[-1] + delta)
         delta_y = vec_deriv(y_arr[-1]) * delta
         y_arr.append(y_arr[-1] + delta_y)
     return x_arr, y_arr
-
-
-
