a468144113f953833dd53a4d8a313e6f75c3d024
JE1532 <jonatanalf2016@gmail.com>
2023-09-28 07:21:15 +0300

    updates.

--- euler_integration.py
+++ euler_integration.py
@@ -1,15 +1,23 @@
 import numpy
 
-def forward_euler_integration(y_0, vec_deriv, delta, conv=None, bounds=None):
+def forward_euler_integration(y_0, vec_deriv, delta, x_0=0, conv=None, bounds=None):
     if bounds == None and conv == None:
         raise Exception("Improper use - need to define end condition for integration: either conv != None or bounds != None")
-    x_arr = [0]
+    x_arr = [x_0]
     y_arr = [y_0]
     get_conv_derivative = lambda y_vals: numpy.array([y[conv] for y in y_vals])
     conv_condition = lambda y_arr: numpy.std(get_conv_derivative(y_arr[-100:])) >= 0.01 * numpy.avg(get_conv_derivative(y_arr[-100:]))
-    counds_condition = lambda x_arr: x_arr[-1] + delta > bounds[1] or x_arr[-1] + delta < bounds[1]
-    while len(y_arr) < 100 or (not (conv and conv_condition(y_arr)) and not (bounds and counds_condition(x_arr))):
+    bounds_condition = lambda x_arr: x_arr[-1] + delta < bounds[1] and x_arr[-1] + delta > bounds[0]
+    i = 0
+    while len(y_arr) < 100 or ((not conv or conv_condition(y_arr)) and (not bounds or bounds_condition(x_arr))):
+        i += 1
+        #print(i)
         x_arr.append(x_arr[-1] + delta)
-        delta_y = vec_deriv(y_arr[-1]) * delta
+        delta_y = vec_deriv(x_arr[-1], y_arr[-1]) * delta
         y_arr.append(y_arr[-1] + delta_y)
     return x_arr, y_arr
+
+
+if __name__ == "__main__":
+    x_arr, y_arr = forward_euler_integration(numpy.array([0, 0, 2]), lambda x, y: numpy.array([y[1], y[2], 0]), 0.0001, bounds=[0, 10])
+    print(y_arr[60000])
--- numerical_static_meniscus.py
+++ numerical_static_meniscus.py
@@ -1,1 +1,25 @@
 import numpy
+import matplotlib.pyplot as plt
+import time
+
+from euler_integration import forward_euler_integration
+
+
+def numerical_static_meniscus(bond_number, pressure_diff, r_0=0.0000001):
+    y_0 = numpy.array([- (pressure_diff / 4) * (r_0 ** 2), - (1 / 2) * pressure_diff * r_0, - pressure_diff * (1 / 2)])
+    sec_deriv_coeff = lambda x, y:  (1 + (y[1] ** 2)) ** (3 / 2)
+    sec_deriv_internal = lambda x, y: - bond_number * y[0] - (y[1] / (x * numpy.sqrt(1 + y[1] ** 2))) - pressure_diff
+    sec_deriv = lambda x, y: sec_deriv_coeff(x, y) * sec_deriv_internal(x, y)
+    vec_deriv = lambda x, y: numpy.array([y[1], y[2], sec_deriv(x, y)])
+    delta = 0.0000001
+    bounds = [0, 1]
+    x_arr, y_arr = forward_euler_integration(y_0, vec_deriv, delta, x_0=r_0, bounds=[0, 1])
+    return x_arr, y_arr
+
+
+if __name__ == "__main__":
+    x_arr, y_arr = numerical_static_meniscus(0, 2)
+    y_arr = [y[0] for y in y_arr]
+    print(y_arr[-1])
+    plt.plot(x_arr, y_arr)
+    plt.show()
